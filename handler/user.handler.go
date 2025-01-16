063db9ef2c3764ac8e448d21e6089e2135281d6d
mailvlous <mailvelous03@gmail.com>
2025-01-16 22:22:44 +0700

    feat: add update user, public asset, delete user

--- handler/user.handler.go
+++ handler/user.handler.go
@@ -6,6 +6,7 @@ import (
 	"restapi-fiber-gorm/model/entity"
 	"restapi-fiber-gorm/database"
 	"restapi-fiber-gorm/model/request"
+	"restapi-fiber-gorm/model/response"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -67,7 +68,7 @@ func CreateUser(ctx *fiber.Ctx) error {
 
 }
 
-func GetDataById(ctx *fiber.Ctx) error {
+func GetUserById(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	var user entity.Users
 
@@ -76,5 +77,84 @@ func GetDataById(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusInternalServerError).SendString(result.Error.Error())
 	}
 
-	return ctx.JSON(user)
+	userResponse := response.UserResponse{
+		Id:        user.Id,
+		Name:      user.Name,
+		Email:     user.Email,
+		Phone:     user.Phone,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+
+	return ctx.JSON(userResponse)
+}
+
+func UserUpdateName(ctx *fiber.Ctx) error {
+	// Get User by ID
+
+	userRequest := new(request.UserUpdateRequest)
+
+	if err := ctx.BodyParser(userRequest); err != nil {
+		return ctx.Status(400).JSON(fiber.Map{
+			"message": "Bad request",
+		})
+	}
+
+	userId := ctx.Params("id")
+	var user entity.Users
+
+	err := database.DB.First(&user, "id = ?", userId).Error
+	if err != nil {
+		return ctx.Status(404).JSON(fiber.Map{
+			"message": "User not found",
+			"data": nil,
+		})
+	}
+
+	// Update user Name or Phone
+
+	user.Name = userRequest.Name
+	if userRequest.Name != "" {
+		user.Name = userRequest.Name
+	}
+
+	errUpdate := database.DB.Save(&user).Error
+	if errUpdate != nil {
+		return ctx.Status(500).JSON(fiber.Map{
+			"message": "Could not update user",
+		})
+	}
+
+	return ctx.JSON(fiber.Map{
+		"status": "success",
+		"message": "User successfully created",
+		"data": user,
+	})
+}
+
+
+func UserDelete(ctx *fiber.Ctx) error {
+	// get user by id
+
+	id := ctx.Params("id")
+	var user entity.Users
+
+	result := database.DB.First(&user, id)
+	if result.Error != nil {
+		return ctx.Status(fiber.StatusInternalServerError).SendString(result.Error.Error())
+	}
+
+	// delete user
+
+	err := database.DB.Delete(&user, id).Error
+	if err != nil {
+		return ctx.Status(500).JSON(fiber.Map{
+			"message": "Could not delete user",
+		})
+	}
+
+	return ctx.JSON(fiber.Map{
+		"status": "success",
+		"message": "User successfully deleted",
+	})
 }
--- route/index.route.go
+++ route/index.route.go
@@ -2,6 +2,8 @@ package route
 
 import (
 	"restapi-fiber-gorm/handler"
+	"restapi-fiber-gorm/config"
+	
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,6 +11,11 @@ import (
 func RouteInit(r *fiber.App) {
 	r.Get("/", handler.HelloWorld)
 	r.Get("/getAllUser", handler.GetAllUser)
+	r.Get("/getUser/:id", handler.GetUserById)
+	r.Put ("/updateUser/:id", handler.UserUpdateName)
+	r.Delete("/deleteUser/:id", handler.UserDelete)
+
+	r.Static("/public", config.ProjectRootPath + "/public/assets")
 
 	r.Post("/createUser", handler.CreateUser)
 }
